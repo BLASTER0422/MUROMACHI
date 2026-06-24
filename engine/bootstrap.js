ba060c55e0e4ae55ce5dbf0824244dcf9cfdd7ae
BLASTER0422 <hakdog020402@gmail.com>
2026-06-24 03:05:58 -0700

    Update bootstrap.js

--- engine/bootstrap.js
+++ engine/bootstrap.js
@@ -1,3 +1,48 @@
-console.log(
-"Muromachi Engine Started"
+(async()=>{
+
+const scripts=[
+
+"./engine/loader.js",
+"./engine/registry.js",
+"./engine/storage.js",
+"./engine/simulation.js"
+
+];
+
+for(
+const s
+of scripts
+){
+
+await new Promise(
+
+(done)=>{
+
+const e=
+document.createElement(
+"script"
 );
+
+e.src=s;
+
+e.onload=done;
+
+document.body.append(
+e);
+
+}
+
+);
+
+}
+
+await StorageEngine.init();
+
+document.getElementById(
+"status"
+)
+.innerHTML=
+
+"✅ Godlike Engine Online";
+
+})();
