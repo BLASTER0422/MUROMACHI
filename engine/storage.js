11134a2bdcef9c8d3e712451027dbf4ef9c02ae5
BLASTER0422 <hakdog020402@gmail.com>
2026-06-24 04:18:48 -0700

    Update storage.js

--- engine/storage.js
+++ engine/storage.js
@@ -1,125 +1,12 @@
 window.StorageEngine={
 
-db:null,
-
 async init(){
 
-return new Promise(
-(resolve,reject)=>{
-
-const request=
-indexedDB.open(
-"MuromachiDB",
-1
-);
-
-request.onupgradeneeded=
-e=>{
-
-const db=
-e.target.result;
-
-if(
-!db.objectStoreNames
-.contains(
-"game"
-)
-){
-
-db.createObjectStore(
-"game"
-);
-
-}
-
-};
-
-request.onsuccess=
-e=>{
-
-this.db=
-e.target.result;
-
 console.log(
-"IndexedDB Ready"
+"Storage Ready"
 );
 
-resolve();
-
-};
-
-request.onerror=
-()=>{
-
-reject(
-"Storage failed"
-);
-
-};
-
-});
-
-},
-
-save(
-key,
-value
-){
-
-return new Promise(
-(resolve)=>{
-
-const tx=
-this.db
-.transaction(
-["game"],
-"readwrite"
-);
-
-tx
-.objectStore(
-"game"
-)
-.put(
-value,
-key
-);
-
-tx.oncomplete=
-resolve;
-
-});
-
-},
-
-load(
-key
-){
-
-return new Promise(
-(resolve)=>{
-
-const tx=
-this.db
-.transaction(
-["game"]
-);
-
-const req=
-tx
-.objectStore(
-"game"
-)
-.get(
-key
-);
-
-req.onsuccess=
-()=>resolve(
-req.result
-);
-
-});
+return true;
 
 }
 
