e1b7e0f5b97402b91b370741d5626eae63bf8690
BLASTER0422 <hakdog020402@gmail.com>
2026-06-24 17:59:35 -0700

    Update bootstrap.js

--- engine/bootstrap.js
+++ engine/bootstrap.js
@@ -1,14 +1,17 @@
 (async()=>{
 
 const status=
-
-document
-.getElementById(
+document.getElementById(
 "status"
 );
 
+const game=
+document.getElementById(
+"game"
+);
+
 status.innerHTML=
-"Loading Engine";
+"Loading Engine...";
 
 async function load(src){
 
@@ -17,22 +20,18 @@ return new Promise(
 (resolve,reject)=>{
 
 const s=
-document
-.createElement(
+document.createElement(
 "script"
 );
 
-s.src=
-src;
+s.src=src;
 
-s.onload=
-resolve;
+s.onload=resolve;
 
 s.onerror=
-reject;
+()=>reject(src);
 
-document.body
-.appendChild(
+document.body.appendChild(
 s
 
 );
@@ -63,19 +62,149 @@ await load("./engine/character.js");
 
 await load("./engine/ui.js");
 
+/*
+START ENGINE
+*/
+
+if(window.StorageEngine)
 await StorageEngine.init();
 
+if(window.WorldEngine)
 await WorldEngine.init();
+
+if(window.PlayerEngine){
 
 await PlayerEngine.init();
 
+PlayerEngine.load();
+
+}
+
+if(window.FactionEngine)
 await FactionEngine.init();
+
+if(window.CharacterEngine){
 
 await CharacterEngine.init();
 
 CharacterEngine.create();
 
-UIEngine.render();
+}
+
+/*
+RENDER WORLD
+*/
+
+let html=`
+
+<div>
+
+<h3>
+Year:
+${
+WorldEngine.world.year
+}
+</h3>
+
+<h3>
+Name:
+${
+WorldEngine.world.name
+}
+</h3>
+
+<h3>
+Villages:
+${
+WorldEngine.world
+.villages
+.join(", ")
+}
+</h3>
+
+</div>
+
+`;
+
+if(
+PlayerEngine.player
+){
+
+html+=`
+
+<hr>
+
+<h2>
+
+🏯 Character
+
+</h2>
+
+<p>
+
+Name:
+${PlayerEngine.player.name}
+
+</p>
+
+<p>
+
+Age:
+${PlayerEngine.player.age}
+
+</p>
+
+<p>
+
+Gender:
+${PlayerEngine.player.gender}
+
+</p>
+
+<p>
+
+Clan:
+${PlayerEngine.player.clan}
+
+</p>
+
+<p>
+
+Gold:
+${PlayerEngine.player.gold}
+
+</p>
+
+<p>
+
+Spouse:
+${
+PlayerEngine.player.family?.spouse
+||
+"None"
+}
+
+</p>
+
+<p>
+
+Children:
+${
+(
+PlayerEngine.player.family?.children
+||
+[]
+).length
+}
+
+</p>
+
+`;
+
+}
+
+game.innerHTML=
+html;
 
 status.innerHTML=
 
@@ -85,7 +214,6 @@ status.innerHTML=
 catch(e){
 
 status.innerHTML=
-
 "❌ Failed";
 
 console.log(e);
