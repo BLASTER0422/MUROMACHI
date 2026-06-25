cec1ee743e9704bc9a140c365ceb4378ad18893d
BLASTER0422 <hakdog020402@gmail.com>
2026-06-24 19:05:49 -0700

    Update player.js

--- engine/player.js
+++ engine/player.js
@@ -1,14 +1,13 @@
 ```javascript
 window.PlayerEngine = {
-
 player:null,
 
 async init(){
 
-try{
-
-const save =
-localStorage.getItem("player");
+let save =
+localStorage.getItem(
+"player"
+);
 
 if(save){
 
@@ -27,23 +26,6 @@ await CharacterEngine.create();
 
 this.render();
 
-document.getElementById(
-"status"
-).innerHTML =
-"✅ Godlike Engine Online";
-
-}
-catch(e){
-
-console.log(e);
-
-document.getElementById(
-"status"
-).innerHTML =
-"❌ Failed";
-
-}
-
 },
 
 async save(){
@@ -60,66 +42,71 @@ this.player
 render(){
 
 if(!this.player){
+
 return;
+
 }
 
-let html = "";
-
-html += "<h2>🏯 Character</h2>";
-
-html += "Name: "
-+ this.player.name
-+ "<br><br>";
-
-html += "Age: "
-+ this.player.age
-+ "<br><br>";
-
-html += "Gender: "
-+ this.player.gender
-+ "<br><br>";
-
-html += "Social Class: "
-+ this.player.socialClass
-+ "<br><br>";
-
-html += "Clan: "
-+ this.player.clan
-+ "<br><br>";
-
-html += "Gold: "
-+ this.player.gold
-+ "<br><br>";
-
-html += "Honor: "
-+ this.player.honor
-+ "<br><br>";
-
-html += "Spouse: "
-+ (
-this.player.spouse
-||
-"None"
-);
-
-html += "<br><br>";
-
-html += "Children: "
-+
-(
-this.player.children
-?
-this.player.children.length
-:
-0
-);
-
-document
-.getElementById(
+document.getElementById(
 "player"
-)
-.innerHTML =
-html;
+).innerHTML =
+
+"<h2>🏯 Character</h2>"
++
+
+"Name: "
++
+this.player.name
+
++
+
+"<br><br>"
+
++
+
+"Age: "
++
+this.player.age
+
++
+
+"<br><br>"
+
++
+
+"Gender: "
++
+this.player.gender
+
++
+
+"<br><br>"
+
++
+
+"Social Class: "
++
+this.player.socialClass
+
++
+
+"<br><br>"
+
++
+
+"Clan: "
++
+this.player.clan
+
++
+
+"<br><br>"
+
++
+
+"Gold: "
++
+this.player.gold;
 
 }
 
